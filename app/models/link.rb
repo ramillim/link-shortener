5aa612f81ef6d294be3bf6e06a8e01fac419aee6
ramillim <ramil.lim@gmail.com>
2018-06-10 19:13:52 -0400

    Validations for custom slugs

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,22 @@
 class Link < ApplicationRecord
+  CHARACTERS_ALLOWED_IN_SLUG = /^[0-9a-zA-Z\-_]*$/
+
   has_many :link_visits
+
+  before_validation :set_random_slug, if: proc { |link| link.slug.nil? }
 
   validates :slug, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true
+  validate :slug_is_url_safe
 
+  private
 
+  def set_random_slug
+    self.slug = SecureRandom.urlsafe_base64(5)
+  end
+
+  def slug_is_url_safe
+    return true if CHARACTERS_ALLOWED_IN_SLUG.match?(slug)
+    errors.add(:slug, 'Custom slug can only include letters, numbers, `-`, and `_`')
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,29 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe Link do
+  let(:long_url) { 'https://some.long.url.com/shorten/me' }
+
   describe 'validations' do
-    let(:url) { 'https://some.long.url.com/shorten/me' }
-    let(:duplicate_slug) { described_class.new(slug: 'link', url: url) }
+    let!(:short_link) { described_class.create!(slug: 'link', url: long_url) }
+    let(:duplicate_slug) { described_class.new(slug: 'link', url: long_url) }
 
     before do
-      described_class.create!(slug: 'link', url: url)
       duplicate_slug.save
     end
 
-    it 'validates the uniqueness of the slug' do
-      expect(duplicate_slug).to be_invalid
-      expect(duplicate_slug.errors[:slug]).to include('has already been taken')
+    describe 'slug' do
+      it 'validates uniqueness' do
+        expect(duplicate_slug).to be_invalid
+        expect(duplicate_slug.errors[:slug]).to include('has already been taken')
+      end
+
+      context 'when a custom slug is provided' do
+        it 'only allows letters and numbers' do
+          short_link.slug = 'this/is/unsafe'
+          short_link.save
+          expect(short_link.errors[:slug])
+            .to include('Custom slug can only include letters, numbers, `-`, and `_`')
+        end
+
+        it 'allows hyphens' do
+          short_link.slug = 'custom-slug'
+          short_link.save
+          expect(short_link).to be_valid
+        end
+
+        it 'allows underscores' do
+          short_link.slug = 'custom_slug'
+          short_link.save
+          expect(short_link).to be_valid
+        end
+      end
     end
 
-    it 'validates the uniqueness of the url' do
-      expect(duplicate_slug).to be_invalid
-      expect(duplicate_slug.errors[:url]).to include('has already been taken')
+    describe 'url' do
+      it 'validates uniqueness' do
+        expect(duplicate_slug).to be_invalid
+        expect(duplicate_slug.errors[:url]).to include('has already been taken')
+      end
     end
   end
 
-  describe 'shortening a url' do
-    it 'encodes a url' do
+  describe 'creating a short link' do
+    context 'when a custom slug is not provided' do
+      let(:link) { described_class.create!(url: long_url) }
 
+      it 'generates a random slug' do
+        expect(link.slug).to be_present
+      end
+
+      it 'generates a slug that is 7 characters long' do
+        expect(link.slug.size).to eq(7)
+      end
+
+      it 'associates the random slug with the url' do
+        expect(link.url).to eq(long_url)
+      end
+    end
+
+    context 'when a custom slug is provided' do
+      let(:link) { described_class.create!(url: long_url, slug: 'custom-slug') }
+
+      it 'associates the custom slug with the url' do
+        expect(link.slug).to eq('custom-slug')
+        expect(link.url).to eq(long_url)
+      end
     end
   end
 end
